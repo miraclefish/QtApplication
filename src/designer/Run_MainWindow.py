0f09a19e65b1d299b35b125fe8e4e8a22b196208
miraclefish <yuyifei@sjtu.edu.cn>
2020-09-29 10:02:00 +0800

    Original

---
+++ src/designer/Run_MainWindow.py
@@ -0,0 +1,14 @@
+import sys
+import Ui_MainWindow
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    mainWindow = QMainWindow()
+    ui = Ui_MainWindow.Ui_MainWindow()
+    ui.setupUi(mainWindow)
+
+    mainWindow.show()
+    sys.exit(app.exec_())
