5691c919b804e6f694eec24db995698c9a16f591
miraclefish <yuyifei@sjtu.edu.cn>
2020-10-01 11:20:12 +0800

    单选框控件

---
+++ src/Control/QRadioButtonDemo.py
@@ -0,0 +1,59 @@
+'''
+QAbstractButton
+
+
+QPushButton
+AToolButton
+QRadioButton
+QCheckBox
+
+'''
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+import sys
+
+class QRadioButtonDemo(QWidget):
+    def __init__(self):
+        super(QRadioButtonDemo, self).__init__()
+        self.initUI()
+    
+    def initUI(self):
+        self.setWindowTitle("QRadioButton控件演示")
+
+        layout = QHBoxLayout(self)
+
+        self.button1 = QRadioButton('单选按钮1')
+        self.button1.setChecked(True)
+        self.button1.toggled.connect(self.buttonState)
+
+        self.button2 = QRadioButton('单选按钮2')
+        self.button2.toggled.connect(self.buttonState)
+
+        layout.addWidget(self.button1)
+        layout.addWidget(self.button2)
+
+    def buttonState(self):
+        radioButton = self.sender()
+        if radioButton.text() == '单选按钮1':
+            if radioButton.isChecked() == True:
+                print('<'+radioButton.text()+'> 被选中')
+            else:
+                print('<'+radioButton.text()+'> 被取消选中')
+        
+        if radioButton.text() == '单选按钮2':
+            if radioButton.isChecked() == True:
+                print('<'+radioButton.text()+'> 被选中')
+            else:
+                print('<'+radioButton.text()+'> 被取消选中')
+
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    main = QRadioButtonDemo()
+    main.show()
+
+    sys.exit(app.exec_())
+        
